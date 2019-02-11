e316804bb384cff479d3369cacf52118a3f1cbc9
nonani42 <42.nonani@gmail.com>
2019-02-12 00:24:56 +0300

    Function added. Изучен процедурный подход.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            DrawDot(x1, y1, sym1);
+            int x2 = 3;
+            int y2 = 5;
+            char sym2 = '#';
+            DrawDot(x2, y2, sym2);
             Console.ReadLine();
+        }
+        static void DrawDot(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
