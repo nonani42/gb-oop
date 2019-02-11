a5e1da0bf1354fcc84bbf52fe68a39449efd4f33
nonani42 <42.nonani@gmail.com>
2019-02-12 00:54:35 +0300

    Создан класс Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,20 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            DrawDot(x1, y1, sym1);
-            int x2 = 3;
-            int y2 = 5;
-            char sym2 = '#';
-            DrawDot(x2, y2, sym2);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw(p1.x, p1.y, p1.sym);
+            Point p2 = new Point();
+            p2.x = 3;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw(p2.x, p2.y, p2.sym);
             Console.ReadLine();
-        }
-        static void DrawDot(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
